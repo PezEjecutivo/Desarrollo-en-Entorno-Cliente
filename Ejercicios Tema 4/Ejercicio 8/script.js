e93abc4c452bd4d002387bb55fbac1fa5e6077e7
PezEjecutivo <119010273+PezEjecutivo@users.noreply.github.com>
2023-11-21 13:02:04 +0100

    Ejercicio 28 escrito enunciado

--- Ejercicios Tema 4/Ejercicio 8/script.js
+++ Ejercicios Tema 4/Ejercicio 8/script.js
@@ -1,11 +1,11 @@
 //Cambiar estilo de la pagina y el boton
-function cambiar(){
-    if(document.getElementById("cuerpo").classList.contains("blanco")){
+function cambiar() {
+    if (document.getElementById("cuerpo").classList.contains("blanco")) {
         document.getElementById("cuerpo").classList.remove("blanco");
         document.getElementById("cuerpo").classList.add("oscuro");
         document.getElementById("cambiarEstilo").classList.remove("btn-dark");
         document.getElementById("cambiarEstilo").classList.add("btn-light");
-    }else{
+    } else {
         document.getElementById("cuerpo").classList.remove("oscuro");
         document.getElementById("cuerpo").classList.add("blanco");
         document.getElementById("cambiarEstilo").classList.remove("btn-light");
@@ -13,3 +13,6 @@ function cambiar(){
     }
 
 }
+
+//Script para los ejercicios
+
